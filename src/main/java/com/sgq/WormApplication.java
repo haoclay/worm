3ae7e5872150882cbd934a1949c37450e9bd091a
sgq <shugq@foxmail.com>
2020-12-22 14:59:10 +0800

    1

--- src/main/java/com/sgq/WormApplication.java
+++ src/main/java/com/sgq/WormApplication.java
@@ -1,9 +1,12 @@
 package com.sgq;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class WormApplication {
 
 	public static void main(String[] args) {
